e5e33210b79ba3f02b0a569ec6c0015b46e6a312
Flavio deSouza <flavio.desouza@alphasights.com>
2021-11-11 17:56:40 -0300

    Using JSX

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement(
-    "lu", 
-    null, 
-    React.createElement("li", null,  "Monday"), // children
-    React.createElement("li", null,  "Tuesday"),
-    React.createElement("li", null,  "Wednesday"),
-  ),
+  <lu>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+  </lu>,
   document.getElementById('root')
 );

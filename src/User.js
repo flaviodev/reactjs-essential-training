e66ef79340633c6e95dac8340d34df20ebf63589
Flavio deSouza <flavio.desouza@alphasights.com>
2021-11-12 09:20:02 -0300

    Handling loading states

--- src/User.js
+++ src/User.js
@@ -4,12 +4,30 @@ import './App.css';
 function User(props) {
   const { login } = props;
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
         .then((response) => response.json())
         .then(setData)
-  }, []);
+        .then(() => setLoading(false))
+        .catch(setError)
+  }, [login]);
+
+  if(loading) return (
+    <>
+        <div className="App">
+            <p>Loading...</p>
+        </div>
+    </>
+  );
+
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+
+  if(!data) return null;
 
   return (
     <div className="App">
